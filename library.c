6563b1cb0b5e94b6ae5e905eb4fc21dd027b867a
martin-alem <alemajohmartin@gmail.com>
2020-09-15 00:29:35 -0400

    implement init function

--- library.c
+++ library.c
@@ -1,4 +1,17 @@
 //
 // Created by Martin Alemajoh on 9/14/2020.
 //
+#include "library.h"
+
+/*
+ * DEF: Initialize SLinkedList
+ * PARAM1: SLinkedList *list - single linkedlist structure to be initialize
+ * PARAM2: char *type - object datatype stored in list
+ * RETURN: returns void
+*/
+void init(SLinkedList *list, char *type){
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+}
 
